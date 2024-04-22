a186cf773ab55b7fd4bc06d25d56a7fd86b240c7
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2024-04-22 09:07:12 +0200

    add visual constructor

--- src/cellier/models/visuals/base_visual.py
+++ src/cellier/models/visuals/base_visual.py
@@ -1,12 +1,18 @@
 """Base classes for visuals and materials."""
 
+from uuid import uuid4
+
 from psygnal import EventedModel
+from pydantic import Field
 
 
 class BaseVisual(EventedModel):
     """Base model for all visuals."""
 
     name: str
+
+    # store a UUID to identify this specific scene.
+    id: str = Field(default_factory=lambda: uuid4().hex)
 
 
 class BaseMaterial(EventedModel):
--- src/cellier/render/render_manager.py
+++ src/cellier/render/render_manager.py
@@ -1,12 +1,28 @@
 """RenderManager class contains all of the rendering and visuals code."""
 
-from typing import Dict
+from typing import Dict, NamedTuple
 
 import pygfx as gfx
 from pygfx.renderers import WgpuRenderer
 from wgpu.gui import WgpuCanvasBase
 
 from cellier.models.viewer import ViewerModel
+from cellier.render.utils import construct_pygfx_object
+
+
+class VisualKey(NamedTuple):
+    """The key to a visual stored in the RenderManager.
+
+    Attributes
+    ----------
+    scene_id : str
+        The uid of the scene model this visual belongs to.
+    visual_id : str
+        The uid of the visual model this pygfx belongs to.
+    """
+
+    scene_id: str
+    visual_id: str
 
 
 class RenderManager:
@@ -16,6 +32,7 @@ class RenderManager:
         # make each scene
         renderers = {}
         scenes = {}
+        visuals = {}
         for scene_model in viewer_model.scenes.scenes:
             # make a renderer
             scene_id = scene_model.id
@@ -25,9 +42,15 @@ class RenderManager:
             scene = gfx.Scene()
             scenes.update({scene_id: scene})
 
+            # populate the scene
+            for visual_model in scene_model.visuals:
+                key = VisualKey(scene_id=scene_model.id, visual_id=visual_model.id)
+                visuals.update({key: construct_pygfx_object(visual_model=visual_model)})
+
         # store the values
         self._renderers = renderers
         self._scenes = scenes
+        self._visuals = visuals
 
     def renderers(self) -> Dict[str, WgpuRenderer]:
         """Dictionary of renderers.
