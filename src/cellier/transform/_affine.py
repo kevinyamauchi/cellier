526e4146337b9e0f223b51fec5c629dfa71b37ee
Kevin <kevin.yamauchi@gmail.com>
2025-07-08 07:49:50 -0700

    fix circular import

--- src/cellier/transform/_affine.py
+++ src/cellier/transform/_affine.py
@@ -5,9 +5,75 @@ from pydantic import ConfigDict, field_validator
 from pydantic_core.core_schema import ValidationInfo
 from typing_extensions import Self
 
-from cellier.transform import BaseTransform
+from cellier.transform._base import BaseTransform
 
 
+# class BaseTransform(EventedModel, ABC):
+#     """Base class for transformations."""
+#
+#     @abstractmethod
+#     def map_coordinates(self, array):
+#         """Apply the transformation to coordinates.
+#
+#         Parameters
+#         ----------
+#         array : np.ndarray
+#             (n, 4) Array to be transformed.
+#         """
+#         raise NotImplementedError
+#
+#     @abstractmethod
+#     def imap_coordinates(self, array):
+#         """Apply the inverse transformation to coordinates.
+#
+#         Parameters
+#         ----------
+#         array : np.ndarray
+#             (n, 4) array to be transformed.
+#         """
+#         raise NotImplementedError
+#
+#     @abstractmethod
+#     def map_normal_vector(self, normal_vector: np.ndarray):
+#         """Apply the transform to a normal vector defining an orientation.
+#
+#         For example, this would be used to a plane normal.
+#
+#         https://www.scratchapixel.com/lessons/mathematics-physics-for-computer-graphics/geometry/transforming-normals.html
+#
+#         Parameters
+#         ----------
+#         normal_vector : np.ndarray
+#             The normal vector(s) to be transformed.
+#
+#         Returns
+#         -------
+#         transformed_vector : np.ndarray
+#             The transformed normal vectors as a unit vector.
+#         """
+#         raise NotImplementedError
+#
+#     @abstractmethod
+#     def imap_normal_vector(self, normal_vector: np.ndarray):
+#         """Apply the inverse transform to a normal vector defining an orientation.
+#
+#         For example, this would be used to a plane normal.
+#
+#         https://www.scratchapixel.com/lessons/mathematics-physics-for-computer-graphics/geometry/transforming-normals.html
+#
+#         Parameters
+#         ----------
+#         normal_vector : np.ndarray
+#             The normal vector(s) to be transformed.
+#
+#         Returns
+#         -------
+#         transformed_vector : np.ndarray
+#             The transformed normal vectors as a unit vector.
+#         """
+#         raise NotImplementedError
+#
+#
 def to_vec4(coordinates: np.ndarray) -> np.ndarray:
     """Convert coordinates to vec4 to make compatible with an affine matrix."""
     coordinates = np.atleast_2d(coordinates)
